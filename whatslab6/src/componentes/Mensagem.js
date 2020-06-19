dd21ad557cb0a5fb659709da15be06cf8291b864
Dienay Lima <dienaylima@gmail.com>
2020-06-19 18:43:42 -0300

    recebendo inputs, e enviando mensagem ao clicar no botão enviar

--- whatslab6/src/componentes/Mensagem.js
+++ whatslab6/src/componentes/Mensagem.js
@@ -1,64 +1,66 @@
 import React from 'react';
 
 class Mensagem extends React.Component {
-    state = {
-        mensagens: [
-          {
-          nomeRemetente: ' ',
-          content: ' '
-        }
-      ], 
-        
-      valorInputMensagem: '',
-      valorInputContent: ''
-    
+  state = {
+    mensagens: [
+      {
+      nomeRemetente: 'Jonvirson',
+      content: 'oi'
+      }
+    ],
+
+    valorInputRemetente: '',
+    valorInputContent: ''
+  }
+
+  onClickEnviar = () => {
+    const novaMensagem = {
+      nomeRemetente: this.state.valorInputRemetente,
+      content: this.state.valorInputContent
     }
     
+    const enviando = [...this.state.mensagens, novaMensagem]
     
-      onChangeInputMensagem = (event) => {
+    this.setState({mensagens: enviando})
+  }
+
+  onChangeInputRemetente = (event) => {
+    this.setState({valorInputRemetente: event.target.value});
+  };
+
+  onChangeInputContent = (event) => {
+    this.setState({valorInputContent: event.target.value});
+  };
+
+  render() {
+
+    const mostraMensagem = this.state.mensagens.map((mensagem) => {
+      return ( 
+      <p>
+        <b>{ mensagem.nomeRemetente }: </b> {mensagem.content}
+      </p>
+      )})
+
+    return (
+      <div className = "App" >
+        <div>
+          { mostraMensagem }
+        </div>
         
-        this.setState({ valorInputMesagem: event.target.value });
-    };
-    
-      onChangeInputContent = (event) => {
-          
-        this.setState({ valorInputContent: event.target.value });
-      };
-
-    render() {
-
-
-
-
-        const mostraMensagem = this.state.mensagens.map((mensagem) => {      
-            return (
-              <p>
-                {mensagem.nomeRemetente} - {mensagem.content}
-              </p>
-            )
-          })
-        
-          return (
-            <div className="App">
-            <input
-            value={ this.state.valorInputMensagem }
-            onChange={ this.onChangeInputMensagem }
-            placeholder={'Digite seu nome'}
-            />
-            <input
-            value={ this.state.valorInputContent }
-            onChange={ this.onChangeInputContent }
-            placeholder={'Sua mensagem'}
-            />
-            <button>
-              Enviar
-            </button>
-          </div> 
-
-        )
-    }
-
-
+        <input
+          value= { this.state.valorInputRemetente }
+          onChange= { this.onChangeInputRemetente }
+          placeholder= {'Digite seu nome'}
+        />
+        <input
+          value= { this.state.valorInputContent }
+          onChange= { this.onChangeInputContent }
+          placeholder= {'Sua mensagem'}
+        />
+        <button onClick={this.onClickEnviar} > Enviar </button>
+      </div> 
+    )
+  }
 }
 
 export default Mensagem
